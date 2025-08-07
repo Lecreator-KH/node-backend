1fc36eb96f88aff2f5f7c3d72940e67d74d232ce
K.Han <44111047+Lecreator-KH@users.noreply.github.com>
2025-08-07 01:29:57 +0000

    Activity 6: Creating more Simple API Endpoints in Node.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.get('/', (req, res) => {
     res.send('Hello from Node.js!');
 });
+
+app.get('/about', (req, res) => {
+    res.send('This API is created by Kevin!');
+});
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
